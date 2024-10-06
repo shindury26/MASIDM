c0d845413943e81526538be38a8306a9e590bc7a
shindury26 <shindury26@gmail.com>
2024-10-06 20:46:10 +0800

    Update MASIDM.cmd
    Added Optimizer App

--- MASIDM.cmd
+++ MASIDM.cmd
@@ -8,7 +8,7 @@
 
 cls
 title  Compilation %blank%OpenSource %blank%Scripts %masver%
-if not defined terminal mode 76, 15
+if not defined terminal mode 76, 18
 
 
 echo:       _______________________________________________________________
@@ -19,17 +19,20 @@ echo:             [3] MS OFFICE Online Downloader 1 Work Only Win10/11
 echo:             [4] MS OFFICE Online Downloader 2 Work Only Win10/11
 echo:             [5] MS OFFICE Online Downloader APP
 echo:             [6] Chris Titus Tech's Windows Utility Work Only Win10/11
+echo:             [7] Optimizer Work Only Win10/11
 echo:             [0] Exit
 echo:       _______________________________________________________________
 echo:
 echo:      Create By: shindury26
-echo:      Credit to the developers of activators and downloaders
+echo:      Credit to the developers of Opensource program/ Script
 echo:      MASSGRAVE ^| lstprjct ^| YerongAI ^| asheroto ^| Chris Titus
+echo:      hellzerg  ^|
 
-choice /C:1234560 /N
+choice /C:12345670 /N
 set _erl=%errorlevel%
 
-if %_erl%==7 exit /b
+if %_erl%==8 exit /b
+if %_erl%==7 start https://github.com/hellzerg/optimizer/releases/download/16.7/Optimizer-16.7.exe & goto MainMenu
 if %_erl%==6 start powershell.exe iex(irm https://christitus.com/win) & goto MainMenu
 if %_erl%==5 start https://github.com/asheroto/Deploy-Office/releases/download/1.0.1/Deploy-Office.exe & goto MainMenu
 if %_erl%==4 start powershell.exe iex(irm https://officetool.plus/get/) & goto MainMenu
