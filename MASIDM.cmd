f13e7785662e15f12ffde92cfd5b9ef19e39ff6a
shindury26 <shindury26@gmail.com>
2024-10-06 20:21:34 +0800

    Update MASIDM.cmd
    added Chris Titus Tech's Windows Utility

--- MASIDM.cmd
+++ MASIDM.cmd
@@ -8,7 +8,7 @@
 
 cls
 title  Compilation %blank%OpenSource %blank%Scripts %masver%
-if not defined terminal mode 76, 13
+if not defined terminal mode 76, 15
 
 
 echo:       _______________________________________________________________
@@ -18,17 +18,19 @@ echo:             [2] Internet Download Manager Reset ^| Freeze
 echo:             [3] MS OFFICE Online Downloader 1 Work Only Win10/11
 echo:             [4] MS OFFICE Online Downloader 2 Work Only Win10/11
 echo:             [5] MS OFFICE Online Downloader APP
+echo:             [6] Chris Titus Tech's Windows Utility Work Only Win10/11
 echo:             [0] Exit
 echo:       _______________________________________________________________
 echo:
 echo:      Create By: shindury26
 echo:      Credit to the developers of activators and downloaders
-echo:      MASSGRAVE ^| lstprjct ^| YerongAI ^| asheroto
+echo:      MASSGRAVE ^| lstprjct ^| YerongAI ^| asheroto ^| Chris Titus
 
-choice /C:123450 /N
+choice /C:1234560 /N
 set _erl=%errorlevel%
 
-if %_erl%==6 exit /b
+if %_erl%==7 exit /b
+if %_erl%==6 start powershell.exe iex(irm https://christitus.com/win) & goto MainMenu
 if %_erl%==5 start https://github.com/asheroto/Deploy-Office/releases/download/1.0.1/Deploy-Office.exe & goto MainMenu
 if %_erl%==4 start powershell.exe iex(irm https://officetool.plus/get/) & goto MainMenu
 if %_erl%==3 start powershell.exe iex(irm https://otp.landian.vip/get/) & goto MainMenu
