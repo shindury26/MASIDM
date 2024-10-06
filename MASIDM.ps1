6f73c1ece7f47619d768ac706c75cfd267f2e692
shindury26 <shindury26@gmail.com>
2024-10-06 17:51:31 +0800

    Create MASIDM.ps1

---
+++ MASIDM.ps1
@@ -0,0 +1,28 @@
+
+
+$ErrorActionPreference = "Stop"
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/shindury26/MASIDM/refs/heads/main/MASIDM.cmd'
+
+$rand = Get-Random -Maximum 99999999
+$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MASIDM_$rand.cmd" } else { "$env:TEMP\MASIDM_$rand.cmd" }
+
+try {
+    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+}
+catch {
+    $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing
+}
+
+$ScriptArgs = "$args "
+$prefix = "@REM $rand `r`n"
+$content = $prefix + $response
+Set-Content -Path $FilePath -Value $content
+
+Start-Process $FilePath $ScriptArgs -Wait
+
+$FilePaths = @("$env:TEMP\MASIDM*.cmd", "$env:SystemRoot\Temp\MASIDM*.cmd")
+foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
